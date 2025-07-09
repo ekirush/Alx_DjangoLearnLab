a29eb525604c06bf47461d88fcf5014b6ca8532a
ekirush <ekirush966@gmail.com>
2025-07-10 01:20:52 +0300

    Django Admin Interface update

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -6,6 +6,7 @@ from .models import Book
 
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'publication_year')
+    list_filter = ('publication_year', 'author')
     search_fields = ('title', 'author')   
 
 admin.site.register(Book, BookAdmin)
